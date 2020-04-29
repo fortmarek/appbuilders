a84a1f5b7cb648b29c6a618ae2372034b2ab5026
Marek Fořt <marek.fort@ackee.cz>
2020-04-29 16:49:18 +0200

    Add progress animation.

--- ranmoji/Sources/RanmojiKit/RanmojiService.swift
+++ ranmoji/Sources/RanmojiKit/RanmojiService.swift
@@ -16,6 +16,15 @@ struct RanmojiService {
         let emojisPath = fileHandler.currentPath.appending(component: "emojis.json")
         let emojisData = try fileHandler.readFile(emojisPath)
         let emojis = try JSONDecoder().decode([Emoji].self, from: emojisData)
+        
+        let progressAnimation = PercentProgressAnimation(stream: stdoutStream.stream, header: "Generating random emojis...")
+        (0...20).forEach { currentStep in
+            let randomEmoji = emojiGenerator.generateRandomEmoji(emojis)
+            progressAnimation.update(step: currentStep, total: 20, text: randomEmoji.emoji)
+            usleep(100_000)
+        }
+        progressAnimation.clear()
+        progressAnimation.complete(success: true)
         let randomEmoji = emojiGenerator.generateRandomEmoji(emojis)
         print("\(randomEmoji.emoji) is your generated random emoji!")
     }
